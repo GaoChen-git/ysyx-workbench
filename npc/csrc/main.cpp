f7c025248f4d763f0edbfbaf04d647da2ca190a4
gaochen <2310169993@qq.com>
2022-12-09 06:08:34 +0800

    NJU-ProjectN/nvboard master initialized without tracing
    NJU-ProjectN/nvboard 68ffd2115a5944b5e616dad34fd1f838fa8e44ed N4: remove unused pins

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,23 @@
 #include <stdio.h>
-
-int main() {
-  printf("Hello, ysyx!\n");
+#include <stdlib.h>
+#include <assert.h>
+#include "Vour_OnOff.h"  //因为创建的是our_OnOff.v 所以头文件要包含Vour_OnOff.h(把V放在前面)
+ 
+ 
+int main(int argc,char **argv)
+{
+  Verilated::commandArgs(argc,argv);
+  Vour_OnOff *top = new Vour_OnOff("top");
+  while(!Verilated::gotFinish())
+  {                                                                                                                                 
+    int a = rand() & 1;
+    int b = rand() & 1;
+    top->a = a;
+    top->b = b;
+    top->eval();
+    printf("a = %d, b = %d, f = %d\n",a,b, top->f);
+    assert(top->f == a ^ b);
+  }
+  delete top;
   return 0;
 }
