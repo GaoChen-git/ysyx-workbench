dd0437d62afb39898a1c5d068d70f3d57191613a
tracer-ysyx2204 <tracer@ysyx.org>
2024-12-05 14:42:06 +0800

    > sim RTL ysyx_22XXXXXXX 高理祥 Linux gaochen-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:42:06 up  4:13,  1 user,  load average: 0.30, 0.66, 0.50

--- npc/obj_dir/Vour_OnOff___024root.h
+++ npc/obj_dir/Vour_OnOff___024root.h
@@ -5,37 +5,37 @@
 #ifndef VERILATED_VOUR_ONOFF___024ROOT_H_
 #define VERILATED_VOUR_ONOFF___024ROOT_H_  // guard
 
-#include "verilated_heavy.h"
-
-//==========
+#include "verilated.h"
 
 class Vour_OnOff__Syms;
 
-//----------
-
-VL_MODULE(Vour_OnOff___024root) {
+class Vour_OnOff___024root final : public VerilatedModule {
   public:
 
-    // PORTS
+    // DESIGN SPECIFIC STATE
     VL_IN8(a,0,0);
     VL_IN8(b,0,0);
     VL_OUT8(f,0,0);
+    CData/*0:0*/ __VactContinue;
+    IData/*31:0*/ __VstlIterCount;
+    IData/*31:0*/ __VicoIterCount;
+    IData/*31:0*/ __VactIterCount;
+    VlTriggerVec<1> __VstlTriggered;
+    VlTriggerVec<1> __VicoTriggered;
+    VlTriggerVec<0> __VactTriggered;
+    VlTriggerVec<0> __VnbaTriggered;
 
     // INTERNAL VARIABLES
-    Vour_OnOff__Syms* vlSymsp;  // Symbol table
+    Vour_OnOff__Syms* const vlSymsp;
 
     // CONSTRUCTORS
-  private:
-    VL_UNCOPYABLE(Vour_OnOff___024root);  ///< Copying not allowed
-  public:
-    Vour_OnOff___024root(const char* name);
+    Vour_OnOff___024root(Vour_OnOff__Syms* symsp, const char* v__name);
     ~Vour_OnOff___024root();
+    VL_UNCOPYABLE(Vour_OnOff___024root);
 
     // INTERNAL METHODS
-    void __Vconfigure(Vour_OnOff__Syms* symsp, bool first);
+    void __Vconfigure(bool first);
 } VL_ATTR_ALIGNED(VL_CACHE_LINE_BYTES);
-
-//----------
 
 
 #endif  // guard
