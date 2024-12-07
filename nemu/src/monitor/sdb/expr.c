dc2eeafe93411e4152eb0c91bcf3cc0801082dd0
tracer-ysyx2204 <tracer@ysyx.org>
2024-12-07 18:17:56 +0800

    > compile NEMU ysyx_22XXXXXXX 高理祥 Linux gaochen-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:17:56 up  7:29,  1 user,  load average: 0.65, 0.30, 0.18

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -75,10 +75,10 @@ void init_regex() {
 
 typedef struct token {
   int type;
-  char str[32];
+  char str[1024];
 } Token;
 
-static Token tokens[32] __attribute__((used)) = {};
+static Token tokens[1024] __attribute__((used)) = {};
 static int nr_token __attribute__((used))  = 0;
 
 static bool make_token(char *e) {
