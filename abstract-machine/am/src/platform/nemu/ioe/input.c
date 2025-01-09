89009b2661ad1d80c8d1cbf70eaff807311e6730
tracer-ysyx2204 <tracer@ysyx.org>
2025-01-09 23:58:43 +0800

    > compile NEMU ysyx_22XXXXXXX 高理祥 Linux gaochen-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:58:43 up  2:56,  1 user,  load average: 0.29, 0.38, 0.49

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -1,9 +1,16 @@
 #include <am.h>
 #include <nemu.h>
 
-#define KEYDOWN_MASK 0x8000
+#define KEYDOWN_MASK 0x8000 // 高位用于标识按下状态
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+    uint32_t key_data = inl(KBD_ADDR); // 从键盘数据寄存器读取按键信息
+
+    if (key_data == AM_KEY_NONE) { // 无按键输入
+        kbd->keydown = false;
+        kbd->keycode = AM_KEY_NONE;
+    } else {
+        kbd->keydown = (key_data & KEYDOWN_MASK) != 0; // 高位判断是否按下
+        kbd->keycode = key_data & ~KEYDOWN_MASK;       // 低位保存键码
+    }
 }
